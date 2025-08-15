58bb30ca0bd146af2c5f108d00c13baff59cdfad
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-08-15 18:05:42 +0530

    secure MongoDB connection using env variables with ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
+// app.module.ts
 import { Module } from '@nestjs/common';
 import { ProductModule } from './api/product/product.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // .env load වෙන්න
+    MongooseModule.forRoot(process.env.MONGODB_URL!), // env variable use
+    ProductModule,
+  ],
 })
 export class AppModule {}
