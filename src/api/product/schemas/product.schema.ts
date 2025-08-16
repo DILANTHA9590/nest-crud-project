d9d3bea47a16890e55cfb89a1020466747fdcd13
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-08-16 22:27:20 +0530

    Add Product Mongoose schema with prescription and date fields for NestJS backend 🛠️

--- src/api/product/schemas/product.schema.ts
+++ src/api/product/schemas/product.schema.ts
@@ -1,40 +1,40 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-
-
-
-
+import { Prop, raw, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 @Schema()
+export default class Product {
+  @Prop({ required: true, unique: true })
+  productId: string;
 
+  @Prop({ required: true })
+  name: string;
 
-export default class Product{
-    @Prop({required:true , unique:true})
-    productId : string;
+  @Prop({ required: true })
+  price: number;
 
+  @Prop({ required: true })
+  sellingPrice: number;
 
-    @Prop({ required: true })
-    name :string
+  @Prop({ required: true })
+  rating: string;
 
+  @Prop({ required: true })
+  available: boolean;
 
+  @Prop(
+    raw([
+      {
+        medicine: { type: String, required: true },
+        dosage: { type: String, required: true },
+        duration: { type: String, required: true },
+        instructions: { type: String },
+        notes: { type: String },
+      },
+    ]),
+  )
+  prescription: Record<string, any>[];
 
-    @Prop({ required: true })
-
-    price: number
-
-
-    @Prop({ required: true })
-
-    sellingPrice: number
-
-
-    @Prop({ required: true })
-    rating: string
-
-
-    @Prop({ required: true })
-    available : boolean
-
-
+  @Prop({ default: Date.now })
+  date_addedd: Date;
 }
 
- export const ProductSchema = SchemaFactory.createForClass(Product)
+export const ProductSchema = SchemaFactory.createForClass(Product);
