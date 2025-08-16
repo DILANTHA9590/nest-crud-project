15702ce39a23713ab95fd0a88ccd8626fdf2fedb
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-08-16 20:58:20 +0530

    create update product dto 🌟🌟

---
+++ src/api/product/dto/update-product-dto.ts
@@ -0,0 +1,7 @@
+import { PartialType } from "@nestjs/mapped-types";
+import { createProductDto } from "./create-product.dto";
+
+// 
+export class updateProductDto extends PartialType(createProductDto){
+
+}
