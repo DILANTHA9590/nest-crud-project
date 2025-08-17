faf7da925f18cc8063d91544de3f90d5a7e962cb
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-08-17 23:48:45 +0530

    🌟

--- src/api/product/product.service.ts
+++ src/api/product/product.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import Product from './schemas/product.schema';
 import mongoose, { Model } from 'mongoose';
@@ -12,7 +17,7 @@ export class ProductService {
     @InjectModel(Product.name) private productModel: Model<Product>,
   ) {}
 
-  async createProduct(productData:createProductDto) {
+  async createProduct(productData: createProductDto) {
     try {
       // 1️⃣ Check if productId exists
       const isHave = await this.productModel.findOne({
@@ -46,166 +51,92 @@ export class ProductService {
     }
   }
 
-//updated product service------------------------------------------------------------>
-  async updateProduct(productId:string,productData:updateProductDto) {
-
-
+  //updated product service------------------------------------------------------------>
+  async updateProduct(productId: string, productData: updateProductDto) {
     try {
-
-
-
-
-
-const updateProduct   = await this.productModel.updateOne({productId:productId}, productData);
-
-
-  
-if(updateProduct.matchedCount  == 0){
-
-
-
-  throw new NotFoundException(`Product updated fail  this ${productId} not found`)
-
-}
-
-
-return {
-
-  message: "Product updated Success"
-}
-
-
-
-
-  
-      
-} catch (error) {
-
-
-   if (error.code === 11000) {
-
-    throw new ConflictException('Product ID already exists');
-    }
-
-    throw error;
-    }
-      
-    }
-
-  
-
-
-
-
-
-
-  
-  async deleteProduct(productId:string) {
-
-
-
-    try {
-      
-
-
-
-      const deleteProduct = await this.productModel.findOneAndDelete({productId});
-
-
-if(!deleteProduct){
-
-  throw new NotFoundException(`Deleted failed , product_ID ${productId} not found`)
-}
-
-
-
-      
-
-return {
-  message :`Product id ${productId} deleted success`,
-}
-
-
-      
-    } catch (error) {
-
-
-
-       if (error.code === 11000) {
-
-    throw new ConflictException('Product ID already exists');
-    }
-
-    throw error;
-      
-    }
-
-
-
-    
-  }
-
-
-  async getAllProduct() {
-
-
-
-    try {
-
-      const getAllProduct = await this.productModel.find();
-
-    return {
-
-      message : "All products",
-      product : getAllProduct
-
-    }
-
-      
-    } catch (error) {
-    if (error.code === 11000) {
-
-    throw new ConflictException('Product ID already exists');
-    }
-
-    throw error;
-      
-    }
-  }
-  async getProductById(productId:string) {
-
-try {
-
-      const getProduct = await this.productModel.findOne({productId:productId});
-
-      if(!getProduct){
-
-
-
-      throw new NotFoundException("no product found this id",productId);
-
+      const updateProduct = await this.productModel.updateOne(
+        { productId: productId },
+        productData,
+      );
+
+      if (updateProduct.matchedCount == 0) {
+        throw new NotFoundException(
+          `Product updated fail  this ${productId} not found`,
+        );
       }
 
-      
-
-    return {
-
-      message : "All products",
-      product : getProduct
-
-    }
-
-      
+      return {
+        message: 'Product updated Success',
+      };
     } catch (error) {
-    if (error.code === 11000) {
+      if (error.code === 11000) {
+        throw new ConflictException('Product ID already exists');
+      }
 
-    throw new ConflictException('Product ID already exists');
+      throw error;
     }
+  }
 
-    throw error;
-      
+  async deleteProduct(productId: string) {
+    try {
+      const deleteProduct = await this.productModel.findOneAndDelete({
+        productId,
+      });
+
+      if (!deleteProduct) {
+        throw new NotFoundException(
+          `Deleted failed , product_ID ${productId} not found`,
+        );
+      }
+
+      return {
+        message: `Product id ${productId} deleted success`,
+      };
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException('Product ID already exists');
+      }
+
+      throw error;
     }
-    
+  }
 
+  async getAllProduct() {
+    try {
+      const getAllProduct = await this.productModel.find();
+
+      return {
+        message: 'All products',
+        product: getAllProduct,
+      };
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException('Product ID already exists');
+      }
+
+      throw error;
+    }
+  }
+  async getProductById(productId: string) {
+    try {
+      const getProduct = await this.productModel.findOne({
+        productId: productId,
+      });
+
+      if (!getProduct) {
+        throw new NotFoundException('no product found this id', productId);
+      }
+
+      return {
+        message: 'All products',
+        product: getProduct,
+      };
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException('Product ID already exists');
+      }
+
+      throw error;
+    }
   }
 }
