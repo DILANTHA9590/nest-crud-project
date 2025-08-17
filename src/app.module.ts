7737a6422e009f250e6892a924faf963ee16d577
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-08-17 23:47:57 +0530

    dd UserModule to AppModule with Mongoose and ConfigModule setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common';
 import { ProductModule } from './api/product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './api/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), // .env load වෙන්න
     MongooseModule.forRoot(process.env.MONGODB_URL!), // env variable use
     ProductModule,
+    UserModule,
   ],
 })
 export class AppModule {}
